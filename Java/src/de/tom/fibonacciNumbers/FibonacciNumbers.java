43c6ece01e76376eb773b8385c8c5699428f82c5
Zemux1613 <42244022+Zemux1613@users.noreply.github.com>
2022-01-27 23:51:10 +0100

    feat: add explicit calculation and goldenRatio aproximation

--- Java/src/de/tom/fibonacciNumbers/FibonacciNumbers.java
+++ Java/src/de/tom/fibonacciNumbers/FibonacciNumbers.java
@@ -4,25 +4,43 @@ import java.util.Scanner;
 
 public class FibonacciNumbers {
 
-	
+
 	public static void main(String[] args) {
-		
-		 int number = 0;
 
-		 Scanner scanner = new Scanner(System.in);
-		 
-		 System.out.println("Please choose a not negativ integer number: ");
-		 number = scanner.nextInt();
-		 
-		 System.out.println("The "+number+" number of the fibinacci numbers is "+ generateFibonacciNumber(number));
-		 scanner.close();
-		
-	}
-	
-	private static int generateFibonacciNumber(int numberOfFibonacciSeries) {
-		   if (numberOfFibonacciSeries == 0 || numberOfFibonacciSeries == 1) return numberOfFibonacciSeries;
+		int number = 0;
 
-		    return generateFibonacciNumber(numberOfFibonacciSeries - 1) + generateFibonacciNumber(numberOfFibonacciSeries - 2);
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Please choose a not negativ integer number: ");
+		number = scanner.nextInt();
+
+		System.out.println("The "+number+" number of the fibinacci numbers is "+ generateFibonacciNumber(number));
+		System.out.println("The "+number+" number of the fibinacci numbers is "+ generateExplicidFibonacci(number));
+		System.out.println("The aproximation on the golden ratio is " + goldenRatio(number));
+		scanner.close();
+
 	}
-	
+
+	private static long generateFibonacciNumber(int numberOfFibonacciSeries) {
+		if (numberOfFibonacciSeries == 0 || numberOfFibonacciSeries == 1) return numberOfFibonacciSeries;
+
+		return generateFibonacciNumber(numberOfFibonacciSeries - 1) + generateFibonacciNumber(numberOfFibonacciSeries - 2);
+	}
+
+	private static double generateExplicidFibonacci(int numberOfFibonacciSeries) {
+
+		double x = 1 / Math.sqrt(5);
+		double y = Math.pow(((1 + Math.sqrt(5)) / 2), numberOfFibonacciSeries);
+		double z = Math.pow(((1 - Math.sqrt(5)) / 2), numberOfFibonacciSeries);
+
+		return x * (y - z);
+
+	}
+
+	private static double goldenRatio(int numberOfFibonacciSeries) {
+
+		return (1.0*generateExplicidFibonacci(numberOfFibonacciSeries)) / generateExplicidFibonacci(numberOfFibonacciSeries-1);
+
+	}
+
 }
