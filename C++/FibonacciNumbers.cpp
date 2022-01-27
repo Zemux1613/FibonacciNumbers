ac8be3a4460bc75e5694ca892b4e6ed7478af3c4
Zemux1613 <42244022+Zemux1613@users.noreply.github.com>
2022-01-27 23:39:56 +0100

    feat: add explicit calculation

--- C++/FibonacciNumbers.cpp
+++ C++/FibonacciNumbers.cpp
@@ -1,22 +1,33 @@
 #include "FibonacciNumbers.h"
 #include <stdio.h>
+#include <Math.h>
 
 int main() {
 
     int number = 0;
 
-    printf("Please choose a not negativ integer number");
+    printf("Please choose a not negativ integer number\n");
 
     scanf_s("%d", &number);
 
-    printf("The %d number of the fibinacci numbers is %d", number, generateFibonacci(number));
-
+    printf("The %d number of the fibinacci numbers is %d\n", number, generateExplicidFibonacci(number));
+    printf("The %d number of the fibinacci numbers is %d\n", number, generateFibonacci(number));
+    
     return 0;
 
 }
 
+long long generateExplicidFibonacci(int numberOfFibonacciSeries) {
 
-int generateFibonacci(int numberOfFibonacciSeries) {
+    double x = 1 / sqrt(5);
+    double y = pow(((1 + sqrt(5)) / 2), numberOfFibonacciSeries);
+    double z = pow(((1 - sqrt(5)) / 2), numberOfFibonacciSeries);
+
+    return x * (y - z);
+
+}
+
+long long generateFibonacci(int numberOfFibonacciSeries) {
 
     if (numberOfFibonacciSeries == 0 || numberOfFibonacciSeries == 1) return numberOfFibonacciSeries;
 
